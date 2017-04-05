88b505861a8818d031c0e787eeced1992d545844
dengbupapapa <meizhanghao1989@qq.com>
2017-04-05 18:15:21 +0800

    单点推送

--- public/index.js
+++ public/index.js
@@ -1,91 +1,96 @@
 window.onload = function() {
 
-  var socket = io.connect();
+    var socket = io.connect();
 
-  socket.on('connect', function() {
+    socket.on('connect', function() {
 
-    console.log('You are connected to Server');
+        console.log('You are connected to Server');
 
-    var nameval = prompt('你的名字：');
-    var userName = document.getElementById('userName');
+        var nameval = prompt('你的名字：');
+        var userName = document.getElementById('userName');
 
-    userName.innerHTML = '欢迎您' + nameval;
-    socket.emit('join', nameval);
+        userName.innerHTML = '欢迎您' + nameval;
+        socket.emit('join', nameval);
 
-  });
+    });
 
-  // socket.on('disconnect', function() {
-  //   console.log('You are disconnect to Server');
-  // });
-  socket.on('announcement', function(msg) { //用户连接提示
-    addMessage(msg.name, msg.text, 'announcement', msg.time);
-  });
+    socket.on('disconnect', function() {
+        console.log('You are disconnect to Server');
+    });
+    socket.on('announcement', function(msg) { //用户连接提示
+        addMessage(msg.name, msg.text, 'announcement', msg.time);
+    });
 
-  socket.on('message', function(msg) { //用户消息
-    addMessage(msg.name, msg.text, 'thirdPerson', msg.time);
-  });
+    socket.on('message', function(msg) { //用户消息
+        addMessage(msg.name, msg.text, 'thirdPerson', msg.time);
+    });
+    // String
 
-  var input = document.getElementById('input');
+    socket.on('String', function(msg) { //单点推送实验
+        console.log(msg);
+    })
 
-  input.onkeydown = function(event) { //chlient 输出
+    var input = document.getElementById('input');
 
-    if (event.keyCode == 13 && this.value) {
+    input.onkeydown = function(event) { //chlient 输出
 
-      var li = addMessage('我', this.value, 'me', getTime());
+        if (event.keyCode == 13 && this.value) {
 
-      socket.emit('text', this.value, function(data) {
+            var li = addMessage('我', this.value, 'me', getTime());
 
-        var didStyle = li.className;
+            socket.emit('text', this.value, function(data) {
 
-        li.className = 'OK ' + didStyle;
-        li.title = data;
+                var didStyle = li.className;
 
-      });
+                li.className = 'OK ' + didStyle;
+                li.title = data;
 
-      this.value = '';
+            });
 
-    }
+            this.value = '';
 
-  }
-
-  function addMessage(name, text, className, time) { //聊天窗添加信息
-
-    var li = document.createElement('li');
-    var b = document.createElement('b');
-    var span = document.createElement('span');
-    var p = document.createElement('p');
-
-    b.innerHTML = name + (!text ? '已连接' : '');
-    span.innerHTML = time;
-    p.innerHTML = text;
-
-    if (text) {
-
-      li.appendChild(b);
-      li.appendChild(span);
-      li.appendChild(p);
-
-    } else {
-
-      li.appendChild(b);
-      li.appendChild(span);
+        }
 
     }
 
-    li.className = className;
+    function addMessage(name, text, className, time) { //聊天窗添加信息
 
-    document.getElementById('message').appendChild(li);
+        var li = document.createElement('li');
+        var b = document.createElement('b');
+        var span = document.createElement('span');
+        var p = document.createElement('p');
 
-    return li;
+        b.innerHTML = name + (!text ? '已连接' : '');
+        span.innerHTML = time;
+        p.innerHTML = text;
 
-  }
+        if (text) {
 
-  function getTime() {
+            li.appendChild(b);
+            li.appendChild(span);
+            li.appendChild(p);
 
-    var myDate = new Date();
+        } else {
 
-    return myDate.getFullYear() + '-' + (myDate.getMonth() + 1) + '-' + myDate.getDate() + ' ' + myDate.getHours() + ':' + myDate.getMinutes() + ':' + myDate.getSeconds();
+            li.appendChild(b);
+            li.appendChild(span);
 
-  }
+        }
+
+        li.className = className;
+
+        document.getElementById('message').appendChild(li);
+
+        return li;
+
+    }
+
+    function getTime() {
+
+        var myDate = new Date();
+
+        return myDate.getFullYear() + '-' + (myDate.getMonth() + 1) + '-' + myDate.getDate() + ' ' + myDate.getHours() + ':' + myDate.getMinutes() + ':' + myDate.getSeconds();
+
+    }
 
 }
--- server.js
+++ server.js
@@ -1,37 +1,87 @@
 var express = require('express');
 var app = express();
-
-app.use(express.static('./public'));
-var server = app.listen(3000);
+var cookieParser = require('cookie-parser');
+var session = require("express-session");
+var RedisStore = require('connect-redis')(session);
+var sessionMiddleware = session({
+    saveUninitialized: true,
+    resave: true,
+    secret: 'secret-session',
+    // store: new RedisStore({
+    //     host: '／',
+    //     port: 6379
+    //   }) // 利用redis存储session
+});
+var server = app.listen(8000, function() {
+    console.log('server is localhost:3000');
+});
 
 var io = require('socket.io');
-var socket = io.listen(server);
+var ios = io.listen(server);
 
-socket.on('connection', function(client) {
+var userKeyForSockets = {};
 
-  client.on('join', function(name) { //登陆时发起提醒用户连接广播出去
-    client.nickname = name;
-    client.broadcast.emit('announcement', {
-      name: name,
-      time: getTime()
+ios.use(function(socket, next) {
+    sessionMiddleware(socket.request, socket.request.res, next);
+});
+
+app.use(cookieParser());
+app.use(sessionMiddleware);
+
+app.use(express.static('./public'));
+
+// ios.use()
+ios.on('connection', function(client) {
+
+    console.log('someone connected');
+
+    userKeyForSockets[client.request.session.demoId] = client.id
+
+    client.on('join', function(name) { //登陆时发起提醒用户连接广播出去
+        client.nickname = name;
+        client.broadcast.emit('announcement', {
+            name: name,
+            time: getTime()
+        });
     });
-  });
 
-  client.on('text', function(text, fn) { //各客户端发来的消息广播出去
-    client.broadcast.emit('message', {
-      name: client.nickname,
-      text: text,
-      time: getTime()
+    client.on('text', function(text, fn) { //各客户端发来的消息广播出去
+        client.broadcast.emit('message', {
+            name: client.nickname,
+            text: text,
+            time: getTime()
+        });
+        fn(getTime());
     });
-    fn(getTime());
-  })
 
+    client.on('disconnect', function(msg) { //关闭移除
+        delete userKeyForSockets[client.request.session.demoId];
+        console.log(userKeyForSockets);
+    });
+
+});
+
+app.get('/', function(req, res, next) {
+    req.session.demoId = req.query.user;
+    res.sendFile(__dirname + '/public/demo.html');
+});
+
+app.get('/chat/:user', function(req, res, next) { //单点推送
+    var user = req.params.user;
+    // console.log(!!userKeyForSockets[user]);
+    if (!!userKeyForSockets[user]) {
+        ios.sockets.sockets[userKeyForSockets[user]].emit('String', {
+            user
+        });
+    }
+    console.log(userKeyForSockets);
+    res.end('我推给了' + user);
 });
 
 function getTime() {
 
-  var myDate = new Date();
+    var myDate = new Date();
 
-  return myDate.getFullYear() + '-' + (myDate.getMonth() + 1) + '-' + myDate.getDate() + ' ' + myDate.getHours() + ':' + myDate.getMinutes() + ':' + myDate.getSeconds();
+    return myDate.getFullYear() + '-' + (myDate.getMonth() + 1) + '-' + myDate.getDate() + ' ' + myDate.getHours() + ':' + myDate.getMinutes() + ':' + myDate.getSeconds();
 
 }
